6cd2b2eb44fb695914cf3333f12f83884bf83994
Michael Gauthier <mike@silverorange.com>
2022-03-05 14:48:30 -0400

    Update ESlint configuration to properly lint JS files

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,3 +1,4 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
 
@@ -9,115 +10,14 @@ module.exports = {
 
   env: {
     es6: true,
-    jest: true,
     node: true,
   },
 
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: 'module',
+    requireConfigFile: false,
   },
-
-  overrides: [
-    {
-      files: ['**/*.ts?(x)'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        warnOnUnsupportedTypeScriptVersion: true,
-      },
-      plugins: ['@typescript-eslint'],
-
-      rules: {
-        'default-case': 'off',
-        'no-dupe-class-members': 'off',
-        'no-undef': 'off',
-        'no-array-constructor': 'off',
-        '@typescript-eslint/no-array-constructor': 'error',
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': [
-          'error',
-          {
-            functions: false,
-            classes: false,
-            variables: false,
-            typedefs: false,
-          },
-        ],
-        'no-unused-expressions': 'off',
-        '@typescript-eslint/no-unused-expressions': [
-          'error',
-          {
-            allowShortCircuit: true,
-            allowTernary: true,
-            allowTaggedTemplates: true,
-          },
-        ],
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          {
-            args: 'none',
-            ignoreRestSiblings: true,
-          },
-        ],
-        'no-useless-constructor': 'off',
-        '@typescript-eslint/no-useless-constructor': 'error',
-
-        '@typescript-eslint/adjacent-overload-signatures': 'error',
-        '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
-        '@typescript-eslint/ban-types': 'error',
-        '@typescript-eslint/naming-convention': [
-          'error',
-          {
-            selector: ['function'],
-            format: ['camelCase'],
-          },
-          {
-            selector: ['memberLike', 'variable'],
-            format: ['camelCase', 'UPPER_CASE', 'snake_case'],
-          },
-          {
-            selector: ['parameter'],
-            format: ['camelCase'],
-            filter: {
-              regex: '^_+$',
-              match: false,
-            },
-          },
-          { selector: 'typeLike', format: ['PascalCase'] },
-        ],
-        '@typescript-eslint/explicit-member-accessibility': [
-          'error',
-          {
-            overrides: {
-              constructors: 'no-public',
-            },
-          },
-        ],
-        '@typescript-eslint/consistent-type-assertions': [
-          'error',
-          { assertionStyle: 'as' },
-        ],
-        '@typescript-eslint/consistent-type-definitions': [
-          'error',
-          'interface',
-        ],
-        '@typescript-eslint/no-empty-interface': 'error',
-        '@typescript-eslint/no-misused-new': 'error',
-        '@typescript-eslint/no-namespace': 'error',
-        '@typescript-eslint/triple-slash-reference': [
-          'error',
-          { types: 'prefer-import' },
-        ],
-        '@typescript-eslint/no-var-requires': 'error',
-        '@typescript-eslint/prefer-function-type': 'error',
-        '@typescript-eslint/prefer-namespace-keyword': 'error',
-        '@typescript-eslint/unified-signatures': 'error',
-      },
-    },
-  ],
 
   rules: {
     'array-callback-return': 'error',
@@ -270,4 +170,105 @@ module.exports = {
     'use-isnan': 'error',
     'valid-typeof': 'error',
   },
+
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint'],
+
+      rules: {
+        'default-case': 'off',
+        'no-dupe-class-members': 'off',
+        'no-undef': 'off',
+        'no-array-constructor': 'off',
+        '@typescript-eslint/no-array-constructor': 'error',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': [
+          'error',
+          {
+            functions: false,
+            classes: false,
+            variables: false,
+            typedefs: false,
+          },
+        ],
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/no-unused-expressions': [
+          'error',
+          {
+            allowShortCircuit: true,
+            allowTernary: true,
+            allowTaggedTemplates: true,
+          },
+        ],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            args: 'none',
+            ignoreRestSiblings: true,
+          },
+        ],
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'error',
+
+        '@typescript-eslint/adjacent-overload-signatures': 'error',
+        '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+        '@typescript-eslint/ban-types': 'error',
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: ['function'],
+            format: ['camelCase'],
+          },
+          {
+            selector: ['memberLike', 'variable'],
+            format: ['camelCase', 'UPPER_CASE', 'snake_case'],
+          },
+          {
+            selector: ['parameter'],
+            format: ['camelCase'],
+            filter: {
+              regex: '^_+$',
+              match: false,
+            },
+          },
+          { selector: 'typeLike', format: ['PascalCase'] },
+        ],
+        '@typescript-eslint/explicit-member-accessibility': [
+          'error',
+          {
+            overrides: {
+              constructors: 'no-public',
+            },
+          },
+        ],
+        '@typescript-eslint/consistent-type-assertions': [
+          'error',
+          { assertionStyle: 'as' },
+        ],
+        '@typescript-eslint/consistent-type-definitions': [
+          'error',
+          'interface',
+        ],
+        '@typescript-eslint/no-empty-interface': 'error',
+        '@typescript-eslint/no-misused-new': 'error',
+        '@typescript-eslint/no-namespace': 'error',
+        '@typescript-eslint/triple-slash-reference': [
+          'error',
+          { types: 'prefer-import' },
+        ],
+        '@typescript-eslint/no-var-requires': 'error',
+        '@typescript-eslint/prefer-function-type': 'error',
+        '@typescript-eslint/prefer-namespace-keyword': 'error',
+        '@typescript-eslint/unified-signatures': 'error',
+      },
+    },
+  ],
 };
